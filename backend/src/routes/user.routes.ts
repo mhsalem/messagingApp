f16c2d6630276feec9698182967558ee41718f6e
salem alrez <salem.seraj@gmail.com>
2025-12-07 17:23:24 +0300

    bug fix

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { forgotPassword, register, requestOtp, resetPassword, verifyOtp, verifyRegester } from "../controllers/user.controller";
+import { forgotPassword, register, requestOtp, resetPassword, verifyOtp, verifyRegister,  } from "../controllers/user.controller";
 
 const router = Router();
 
 router.post("/register",register );
-router.post("/verifyReg",verifyRegester);
+router.post("/verifyReg",verifyRegister);
 router.post("/requestOtp", requestOtp);
 router.post("/verifyOtp", verifyOtp);
 router.post("/forgotPassword", forgotPassword);
